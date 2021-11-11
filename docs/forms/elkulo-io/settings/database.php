361bf1c8634793a6d6f6743770d71e02228cedd0
A.Sudo <50855593+elkulo@users.noreply.github.com>
2021-11-11 20:33:51 +0900

    update

--- docs/forms/elkulo-io/settings/database.php
+++ docs/forms/elkulo-io/settings/database.php
@@ -1,23 +1,31 @@
 <?php
 /**
- * データーベースの設定
- * 
+ * データベースの設定
  */
 return [
 
+  // データベースの種類
   'DB_DRIVER' => env('DB_DRIVER'),
 
+  // データベースのホスト名
   'DB_HOST' => env('DB_HOST'),
 
+  // データベースの名前
   'DB_NAME' => env('DB_NAME'),
 
+  // データベースのユーザー名
   'DB_USER' => env('DB_USER'),
 
+  // データベースのパスワード
   'DB_PASSWORD' => env('DB_PASSWORD'),
 
+  // データベースの接頭辞
   'DB_PREFIX' => env('DB_PREFIX'),
 
+  // データベースの文字セット
   'DB_CHARSET' => env('DB_CHARSET'),
 
+  // データベースの照合順序
   'DB_COLLATE' => env('DB_COLLATE'),
+
 ];
