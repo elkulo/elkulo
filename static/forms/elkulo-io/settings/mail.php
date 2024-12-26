de05d241d495cf76e74e701ebd7923de86185fac
A.Sudo <50855593+elkulo@users.noreply.github.com>
2024-12-26 15:48:47 +0900

    update

---
+++ static/forms/elkulo-io/settings/mail.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * SMTPサーバーの設定
+ */
+return [
+
+  // メーラータイプ
+  'MAILER_DRIVER' => env('MAILER_DRIVER'),
+
+  // SMTPサーバー
+  'SMTP_HOST' => env('SMTP_HOST'),
+
+  // SMTPメールアドレス(配信元)
+  'SMTP_MAILADDRESS' => env('SMTP_MAILADDRESS'),
+
+  // メールユーザー名(アカウント名)
+  'SMTP_USERNAME' => env('SMTP_USERNAME'),
+
+  // メールパスワード
+  'SMTP_PASSWORD' => env('SMTP_PASSWORD'),
+
+  // SMTPプロトコル(sslまたはtls)
+  'SMTP_ENCRYPT' => env('SMTP_ENCRYPT'),
+
+  // 送信ポート(465 or 587)
+  'SMTP_PORT' => env('SMTP_PORT'),
+
+  // 送信者の表示名(デフォルトはサイト名)
+  'FROM_NAME' => env('SITE_TITLE'),
+
+  // 管理者メールアドレス
+  'ADMIN_MAIL' => env('ADMIN_MAIL'),
+
+  // 管理者メールCC
+  'ADMIN_CC' => env('ADMIN_CC'),
+
+  // 管理者メールBCC
+  'ADMIN_BCC' => env('ADMIN_BCC'),
+
+];
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicDir: 'static',
+  build: {
+    outDir: 'public',
+  },
   plugins: [
     react(),
     {
