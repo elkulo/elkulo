e7c1f670f974af4a4012a6d706350c76ff758402
A.Sudo <50855593+elkulo@users.noreply.github.com>
2024-12-27 08:27:16 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,5 +19,10 @@ export default defineConfig({
         }
       },
     },
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      modernPolyfills: true,
+      renderLegacyChunks: false,
+    }),
   ],
 });
